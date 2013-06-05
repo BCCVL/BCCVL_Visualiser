f7a69269265f17b34d3ed366b09b13276428d07d
Robert Pyke <robert.j.pyke@gmail.com>
2013-06-05 13:00:10 +1000

    Removes unless check for plone directory

--- puppet/manifests/yum_update.pp
+++ puppet/manifests/yum_update.pp
@@ -4,7 +4,6 @@ class yum_update {
   exec { 'yum update':
     command => 'yum update -y',
     path    => '/usr/local/bin/:/usr/bin/:/bin/',
-    unless  => 'test -d Plone',
     timeout => 0,                                # Disable the command timeout
   }
 
