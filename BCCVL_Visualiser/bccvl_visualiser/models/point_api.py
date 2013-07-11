c2ec73a01d2af9fe38b27f309e7eb8ced2b888c2
Robert Pyke <robert.j.pyke@gmail.com>
2013-07-11 16:51:16 +1000

    Started to add the point api model

---
+++ BCCVL_Visualiser/bccvl_visualiser/models/point_api.py
@@ -0,0 +1,54 @@
+import mapscript
+import os
+
+from bccvl_visualiser.models import TextWrapper
+
+from bccvl_visualiser.models.api import (
+    BaseAPI
+    )
+
+class BasePointAPI(BaseAPI):
+
+    @staticmethod
+    def identifier():
+        return "point"
+
+    @staticmethod
+    def description():
+        desc = """\
+The point API is responsible for processing \
+point data in CSV files.\
+"""
+        return desc
+
+class PointAPIv1(BasePointAPI):
+    """ v1 of the Point API"""
+
+    DEFAULT_LAYER_NAME='DEFAULT'
+
+    MAP_FILE_PATH = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "..",
+        "map_files",
+        "point_api_v1_map_file.map"
+    )
+
+    @staticmethod
+    def version():
+        return 1
+
+    @classmethod
+    def to_dict(_class):
+        """ Returns a dict representation of the API
+
+            This dict identifies the API, and its
+            general purpose. More specific information about the
+            API is defined at the specific version level of the API.
+        """
+        return_dict = {
+            'name':         _class.identifier(),
+            'description':  _class.description(),
+            'version': _class.version()
+        }
+
+        return return_dict
