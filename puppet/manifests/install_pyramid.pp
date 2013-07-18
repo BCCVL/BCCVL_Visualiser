2d3f97f6746d744180e5ee8f455e31dbdfa44c5c
Robert Pyke <robert.j.pyke@gmail.com>
2013-07-18 16:20:56 +1000

    Removes old virtual env setup code

--- puppet/manifests/install_pyramid.pp
+++ puppet/manifests/install_pyramid.pp
@@ -1,24 +1,5 @@
 class install_pyramid {
 
-  # setup virtualenv
-  #exec { 'virtualenv --no-site-packages env':
-  #  command => 'virtualenv --no-site-packages env',
-  #  cwd     => '/vagrant',
-  #  path    => '/usr/local/bin/:/usr/bin/:/bin/',
-  #  timeout => 10 * 60,                        # Allow 10 mins to install
-  #  creates => '/vagrant/env',
-  #}
-
-  # install the pyramid libraries
-  #exec { 'easy_install pyramid':
-  #  command => 'easy_install pyramid',
-  #  cwd     => '/vagrant/env/bin',
-  #  path    => '/vagrant/env/bin/',
-  #  timeout => 120 * 60,                        # Allow 2 hours to install
-  #  require => Exec['virtualenv --no-site-packages env'],
-  #  creates => '/vagrant/env/bin/pcreate',
-  #}
-
   exec { 'python2.7 bootstrap.py':
     cwd     => '/vagrant/BCCVL_Visualiser',
     path    => '/vagrant/env/bin/',
