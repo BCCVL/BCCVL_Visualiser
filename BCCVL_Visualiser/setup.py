bff3bad157ffe32b404e8a366cb08d85c5a3f88e
Robert Pyke <robert.j.pyke@gmail.com>
2013-07-18 21:40:55 +1000

    Removes unnecessary packages. Removes psycopg2 egg.

--- BCCVL_Visualiser/setup.py
+++ BCCVL_Visualiser/setup.py
@@ -8,7 +8,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
-    'psycopg2',
+#    'psycopg2',
 #    'SQLAlchemy',
 #    'GeoAlchemy2',
 
--- puppet/manifests/centos_dependencies.pp
+++ puppet/manifests/centos_dependencies.pp
@@ -45,54 +45,12 @@ class centos_dependencies{
     ensure => present,
   }
 
-  package { "sqlite-devel":
-    ensure => present,
-  }
-
   package { "tk-devel":
-    ensure => present,
-  }
-
-  package { "python-devel.x86_64":
     ensure => present,
   }
 
   package { "libjpeg-turbo-devel.x86_64":
     ensure => present,
-  }
-
-  # Allows for 'import setuptools' in python
-  package { "python-setuptools.noarch":
-    ensure    => present,
-  }
-
-  # Install virtualenv package
-  exec { 'easy_install virtualenv':
-    command => 'easy_install virtualenv',
-    path    => '/usr/local/bin/:/usr/bin/:/bin/',
-    require => Package['python-setuptools.noarch'],
-  }
-
-  # Install Extra Packages for Enterprise Linux repos
-  package { 'epel-release-6-8':
-    ensure => present,
-    source => 'http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm',
-    provider => rpm,
-  }
-
-  # Install Enterprise Linux GIS repos
-  package { 'elgis-release-6-6_0':
-    ensure => present,
-    source => 'http://elgis.argeo.org/repos/6/elgis-release-6-6_0.noarch.rpm',
-    provider => rpm,
-  }
-
-
-  # Install PostgreSQL and PostGIS repos
-  package { 'pgdg-centos92-9.2-6':
-    ensure => present,
-    source => 'http://yum.postgresql.org/9.2/redhat/rhel-6-x86_64/pgdg-centos92-9.2-6.noarch.rpm',
-    provider => rpm,
   }
 
 }
