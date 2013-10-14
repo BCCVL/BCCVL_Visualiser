09c0db677fb8a9e6c87a08b081560a5763705b00
Robert Pyke <robert.j.pyke@gmail.com>
2013-10-14 15:02:32 +1000

    Added constructor for data_id and data_url for data_manager

--- BCCVL_Visualiser/bccvl_visualiser/models/external_api/data_manager.py
+++ BCCVL_Visualiser/bccvl_visualiser/models/external_api/data_manager.py
@@ -6,7 +6,7 @@ class DataManager(object):
 
     @classmethod
     def configure_from_config(class_, settings):
-        """ configure the BCCVL Map constants """
+        """ configure the DataManager constants """
         log = logging.getLogger(__name__)
 
         if (class_.BASE_URL is not None):
@@ -15,5 +15,21 @@ class DataManager(object):
 
         class_.BASE_URL = settings['bccvl.data_manager.base_url']
 
-    def __init__(self):
+    def __init__(self, data_id=None, data_url=None):
+        """ initialise the map instance from a data_url """
+
+        if data_id and data_url:
+            raise ValueError("The DataManager API can't be provided a data_id and a data_url (there can be only one)")
+        elif data_id:
+            self._init_from_data_id(data_id)
+        elif data_url:
+            self._init_from_data_url(data_url)
+        else:
+            raise ValueError("A DataManager must be provided a data_id or a data_url.")
+
+    def _init_from_data_id(self, data_id):
+        self.data_id = data_id
+        raise NotImplementedError("data_id is not yet supported")
+
+    def _init_from_data_url(self, data_url):
         pass
--- BCCVL_Visualiser/bccvl_visualiser/tests/test_data_manager.py
+++ BCCVL_Visualiser/bccvl_visualiser/tests/test_data_manager.py
@@ -23,3 +23,14 @@ class TestBCCVLMap(unittest.TestCase):
 
     def test_data_manager_base_url(self):
         self.assertEqual(DataManager.BASE_URL, self.config['bccvl.data_manager.base_url'])
+
+    def test_new_data_manager_raises_on_bad_args(self):
+        with self.assertRaises(ValueError):
+            DataManager()
+        with self.assertRaises(ValueError):
+            DataManager(data_id="111", data_url="http://example.com")
+
+    def test_new_data_manager_from_data_id(self):
+        # This isn't implemented yet
+        with self.assertRaises(NotImplementedError):
+            my_map = DataManager(data_id="908h08h")
