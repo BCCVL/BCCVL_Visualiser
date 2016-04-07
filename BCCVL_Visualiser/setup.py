1b044c0d5df45679d67e68344260903654c84080
Gerhard Weis <g.weis@griffith.edu.au>
2016-04-07 13:38:52 +1000

    advance dev version

--- BCCVL_Visualiser/setup.py
+++ BCCVL_Visualiser/setup.py
@@ -42,7 +42,7 @@ requires = [
     ]
 
 setup(name='BCCVL_Visualiser',
-      version='1.7.4.dev',
+      version='1.8.0.dev',
       description='BCCVL_Visualiser',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
