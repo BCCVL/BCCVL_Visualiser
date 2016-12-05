6d778810fcfb6cfa2cdd635da988fe6771c00438
Gerhard Weis <g.weis@griffith.edu.au>
2016-12-06 01:12:16 +1000

    release 1.8.0

--- BCCVL_Visualiser/setup.py
+++ BCCVL_Visualiser/setup.py
@@ -42,7 +42,7 @@ requires = [
 ]
 
 setup(name='BCCVL_Visualiser',
-      version='1.8.0rc0',
+      version='1.8.0',
       description='BCCVL_Visualiser',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
