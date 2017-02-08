eba672caf1db733c66b5e0015c601530ce7a1e88
Gerhard Weis <g.weis@griffith.edu.au>
2017-02-09 09:46:41 +1000

    clean up dependencies in setup.py support python setup.py test/nosetests command

--- BCCVL_Visualiser/setup.py
+++ BCCVL_Visualiser/setup.py
@@ -2,67 +2,57 @@ import os
 
 from setuptools import setup, find_packages
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+here = os.path.dirname(__file__)
+
+README = open(os.path.join(here, 'README')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid >= 1.4.4',
-    #    'psycopg2',
-    #    'SQLAlchemy',
-    #    'GeoAlchemy2',
-
-
-    # mapscript and GDAL is now installed during the buildout.
-    # it is installed using the cmmi recipe (configure && make && make install)
-    #
-    # For travis, it's installed via packages
-    #
-    'GDAL >= 1.7.3',
-    #'MapScript >= 7.0.0',      # There is no distirbution we can deppend on.. it's either there ore not
-    'zope.interface >= 4.1.0',
-
-    'dogpile.cache',    # cache regions, lets you cache the result of queries
-    'Pillow',           # Python Imaging Library
-    'shapely',          # PostGIS-ish operations in python
-    'transaction',
-    'pyramid_tm',
-    'pyramid_debugtoolbar',
-    'zope.sqlalchemy',
+    'pyramid',
+    'SQLAlchemy',
+    #'GDAL >= 1.7.3',       # Better to depend on system installed version
+    #'MapScript >= 7.0.0',  # There is no distirbution we can deppend on.. it's either there ore not
+    'dogpile.cache',        # cache regions, lets you cache the result of queries
     'requests',
-    'waitress',
     'pyramid_xmlrpc',
-    'numpy',
-    'matplotlib',
-    'scipy',
-    'repoze.vhm',       # Use repoze middleware for Virtual Host management
-    'csvvalidator',
     'futures',
-    'org.bccvl.movelib [http]',
+    'org.bccvl.movelib[http]',
 ]
 
-setup(name='BCCVL_Visualiser',
-      version='1.8.0',
-      description='BCCVL_Visualiser',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Pyramid",
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-      ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='bccvl_visualiser',
-      install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = bccvl_visualiser:main
-      [console_scripts]
-      """,
-      )
+# FIXME: tests need /tmp/bccvl/mapdata???
+tests_require = [
+    'mock',
+    'nose',
+    'webtest'
+]
+
+setup(
+    name='BCCVL_Visualiser',
+    #setup_requires=['gu_scm_version'],
+    #gu_scm_version=True,
+    version='1.8.0.dev0',
+    description='BCCVL_Visualiser',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web wsgi bfg pylons pyramid',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=tests_require,
+    test_suite='nose.collector',
+    extras_require={'test': tests_require},
+    entry_points={
+        'paste.app_factory': [
+            'main = bccvl_visualiser:main'
+        ],
+    },
+)
